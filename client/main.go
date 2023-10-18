fbef89b0eff77a87db79ac2aa95ad048cf276246
fiemon <yuyamano1206@gmail.com>
2023-10-18 18:27:30 +0900

    Unary RPC

---
+++ client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+
+	"grpc-lesson/pb"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewFileServiceClient(conn)
+	callListFiles(client)
+}
+
+func callListFiles(client pb.FileServiceClient) {
+	res, err := client.ListFiles(context.Background(), &pb.ListFilesRequest{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(res.GetFilenames())
+}
